9bc0370363bb3ef51e09513ca9b54ab95006ff1c
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-11-06 09:36:09 -0700

    Load certificate and fix error handling.

--- Makefile
+++ Makefile
@@ -1,6 +1,15 @@
-PROG=		ssl_server
-WARNINGS=	yes
-LDADD=		-lssl -lcrypto
-DPADD=		${LIBSSL} ${LIBCRYPTO}
+PROG =		ssl_server
+WARNINGS =	yes
+LDADD =		-lssl -lcrypto
+DPADD =		${LIBSSL} ${LIBCRYPTO}
+
+run-regress-${PROG}: 127.0.0.1.crt
+
+# create certificates for TLS
+
+CLEANFILES +=	127.0.0.1.crt 127.0.0.1.key
+
+127.0.0.1.crt:
+	openssl req -batch -new -subj /L=OpenBSD/O=tls-regress/OU=ssl_server/CN=127.0.0.1/ -nodes -newkey rsa -keyout 127.0.0.1.key -x509 -out $@
 
 .include <bsd.regress.mk>
--- ssl_server.c
+++ ssl_server.c
@@ -10,6 +10,7 @@
 
 void print_ciphers(STACK_OF(SSL_CIPHER) *);
 void print_sockname(BIO *);
+void err_ssl(int, const char *, ...);
 
 int
 main(int argc, char *argv[])
@@ -18,45 +19,47 @@ main(int argc, char *argv[])
 	SSL_CTX *ctx;
 	SSL *ssl;
 	BIO *bio;
-	unsigned long error;
 
 	SSL_load_error_strings();
 
+	/* setup method and context */
 	method = TLS_server_method();
 	if (method == NULL)
-		err(1, "TLS_server_method");
+		err_ssl(1, "TLS_server_method");
 	ctx = SSL_CTX_new(method);
 	if (ctx == NULL)
-		err(1, "SSL_CTX_new");
+		err_ssl(1, "SSL_CTX_new");
 	ssl = SSL_new(ctx);
 	if (ssl == NULL)
-		err(1, "SSL_new");
+		err_ssl(1, "SSL_new");
+
+	/* load server certificate */
+	if (SSL_CTX_use_certificate_file(ctx, "127.0.0.1.crt",
+	    SSL_FILETYPE_PEM) <= 0)
+		err_ssl(1, "SSL_CTX_use_certificate_file");
+	if (SSL_CTX_use_PrivateKey_file(ctx, "127.0.0.1.key",
+	    SSL_FILETYPE_PEM) <= 0)
+		err_ssl(1, "SSL_CTX_use_PrivateKey_file");
+	if (SSL_CTX_check_private_key(ctx) <= 0)
+		err_ssl(1, "SSL_CTX_check_private_key");
+
 	print_ciphers(SSL_CTX_get_ciphers(ctx));
 
 	bio = BIO_new_accept("127.0.0.1:12345");
 	if (bio == NULL)
-		err(1, "BIO_new_accept");
-	if (BIO_do_accept(bio) <= 0) {
-		ERR_print_errors(bio);
-		err(1, "BIO_do_accept setup");
-	}
+		err_ssl(1, "BIO_new_accept");
+	if (BIO_do_accept(bio) <= 0)
+		err_ssl(1, "BIO_do_accept setup");
 	print_sockname(bio);
-	if (BIO_do_accept(bio) <= 0) {
-		ERR_print_errors(bio);
-		err(1, "BIO_do_accept wait");
-	}
+	if (BIO_do_accept(bio) <= 0)
+		err_ssl(1, "BIO_do_accept wait");
 
 	SSL_set_bio(ssl, bio, bio);
-	if (SSL_accept(ssl) <= 0) {
-		error = ERR_get_error();
-		fprintf(stderr, "%s\n", ERR_error_string(error, NULL));
-		errx(1, "SSL_accept");
-	}
+	if (SSL_accept(ssl) <= 0)
+		err_ssl(1, "SSL_accept");
 
-	if (BIO_free(bio) <= 0) {
-		ERR_print_errors(bio);
-		err(1, "BIO_free");
-	}
+	if (BIO_free(bio) <= 0)
+		err_ssl(1, "BIO_free");
 	SSL_free(ssl);
 	SSL_CTX_free(ctx);
 
@@ -81,10 +84,8 @@ print_sockname(BIO *bio)
 	char host[NI_MAXHOST], port[NI_MAXSERV];
 	int fd;
 
-	if (BIO_get_fd(bio, &fd) <= 0) {
-		ERR_print_errors(bio);
-		err(1, "BIO_get_fd");
-	}
+	if (BIO_get_fd(bio, &fd) <= 0)
+		err_ssl(1, "BIO_get_fd");
 	slen = sizeof(lsock);
 	if (getsockname(fd, (struct sockaddr *)&lsock, &slen) == -1)
 		err(1, "getsockname");
@@ -92,4 +93,15 @@ print_sockname(BIO *bio)
 	    sizeof(host), port, sizeof(port), NI_NUMERICHOST | NI_NUMERICSERV))
 		errx(1, "getnameinfo");
 	printf("listen sock: %s %s\n", host, port);
+}
+
+void
+err_ssl(int eval, const char *fmt, ...)
+{
+	va_list ap;
+
+	ERR_print_errors_fp(stderr);
+	va_start(ap, fmt);
+	verrx(eval, fmt, ap);
+	va_end(ap);
 }
